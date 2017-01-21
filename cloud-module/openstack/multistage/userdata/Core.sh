9a5047140fc2e1486d2755c7a25c2c6ad07f67c7
Alberto Rocha <arocha@ptinovacao.pt>
2017-01-21 18:25:15 +0000

    deploy infra for a multistage platform

--- cloud-module/openstack/multistage/userdata/Core.sh
+++ cloud-module/openstack/multistage/userdata/Core.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-{{ epel_tarball }}.noarch.rpm
+sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
 sudo yum update 
 sudo yum install ansible -y
 sudo yum install git -y
